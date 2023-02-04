4fbbf24f890bd8576e5dc1965c5379730dc8f794
darkk99 <colerabold@gmail.com>
2023-02-04 16:31:32 +0000

    fix symlinks

---
+++ .bashrc
@@ -0,0 +1,70 @@
+# ~/.bashrc
+
+# ls aliases
+alias lls='\ls'
+alias ls='exa --icons'
+alias la='exa -a --icons'
+alias lt='exa -T --icons'
+alias lat='exa -aT --icons'
+# start display manager
+alias startgui='sudo systemctl start display-manager'
+# copy to clipboard
+alias clip='wl-copy'
+alias paste='wl-paste'
+copy() {
+  if [[ ! -e "$*" ]]; then
+    echo "$*: Does not exist"
+    return 1;
+  elif [[ -d "$*" ]]; then
+    echo "$*: Is a directory"
+    return 1;
+  else
+    cat $* | wl-copy
+  fi
+}
+# refresh .bashrc
+alias rl="source ~/.bashrc"
+
+# Prompt
+function timer_now {
+  date +%s%N
+}
+
+function timer_start {
+  timer_start=${timer_start:-$(timer_now)}
+}
+
+function timer_stop {
+  local delta_us=$((($(timer_now) - $timer_start) / 1000))
+  local us=$((delta_us % 1000))
+  local ms=$(((delta_us / 1000) % 1000))
+  local s=$(((delta_us / 1000000) % 60))
+  local m=$(((delta_us / 60000000) % 60))
+  local h=$((delta_us / 3600000000))
+  # Goal: always show around 3 digits of accuracy
+  if ((h > 0)); then timer_show=${h}h${m}m
+  elif ((m > 0)); then timer_show=${m}m${s}s
+  elif ((s >= 10)); then timer_show=${s}.$((ms / 100))s
+  elif ((s > 0)); then timer_show=${s}.$(printf %03d $ms)s
+  elif ((ms >= 100)); then timer_show=${ms}ms
+  elif ((ms > 0)); then timer_show=${ms}.$((us / 100))ms
+  else timer_show=${us}us
+  fi
+  unset timer_start
+}
+
+trap 'timer_start' DEBUG
+PROMPT_COMMAND=timer_stop
+
+# Directory name
+PS1='\n\[\033[01;40m\]\[\033[01;37m\] \w '
+# End powerline section
+PS1=$PS1'\[\033[01;100m\]\[\033[01;30m\]'
+# Amount of time last command took
+PS1=$PS1'\[\033[01;37m\] took ${timer_show} '
+# Clear background color and text color
+PS1=$PS1'\[\033[01;0m\]'
+# End powerline section
+PS1=$PS1'\[\033[01;95m\]\[\033[01;90m\]'
+# Finally, reset colors + end of prompt
+PS1=$PS1'\[\033[01;0m\] '
---
+++ .config/nvim/init.lua
@@ -0,0 +1,95 @@
+-- ~/.config/nvim/init.lua
+os.execute([[ /usr/bin/env bash 'rm ~/.local/state/nvim/*' ]])
+vim.cmd([[
+" Plugins "
+  call plug#begin()
+  Plug 'tpope/vim-commentary'
+  Plug 'ntpeters/vim-better-whitespace'
+  Plug 'tpope/vim-surround'
+  Plug 'jiangmiao/auto-pairs'
+  Plug 'lukas-reineke/indent-blankline.nvim'
+  Plug 'LnL7/vim-nix'
+  Plug 'https://github.com/ellisonleao/gruvbox.nvim'
+  Plug 'https://github.com/theHamsta/nvim-semantic-tokens'
+  " Plug 'rose-pine/neovim', {'as': 'rose-pine', 'tag': 'v1.*'}
+  Plug 'catppuccin/nvim', { 'as': 'catppuccin' }
+  Plug 'vim-airline/vim-airline'
+  call plug#end()
+" Tab Size (2) "
+  set tabstop=2
+  set softtabstop=2
+  set shiftwidth=2
+  set expandtab
+" Appearance "
+  let &t_ut=''
+  let g:airline_powerline_fonts = 1
+  set background=dark
+  set termguicolors
+  set number
+  set showcmd
+  set encoding=utf-8
+  set relativenumber
+" Disable C-Z "
+  noremap <C-z> <Nop>
+" Better j and k "
+  nnoremap j gj
+  nnoremap k gk
+" Allows you to use esc to use normal-terminal mode by pressing ESC "
+  tnoremap <Esc> <C-\><C-n>
+  tnoremap <M-[> <Esc>
+  tnoremap <C-v><Esc> <Esc>
+" Disables mouse "
+  set mouse =
+" Opens new terminal window (vertically split) "
+  noremap <C-q> :vs<Enter><C-w>l:term<Enter>
+]])
+vim.api.nvim_create_autocmd("ColorScheme", {
+  command = [[
+    highlight IndentBlanklineIndent1 guifg=#eb6f92 gui=nocombine
+    highlight IndentBlanklineIndent2 guifg=#f6c177 gui=nocombine
+    highlight IndentBlanklineIndent3 guifg=#9ccfd8 gui=nocombine
+    highlight IndentBlanklineIndent4 guifg=#31748f gui=nocombine
+    highlight IndentBlanklineIndent5 guifg=#c4a7e7 gui=nocombine
+  ]]
+})
+
+vim.g.strip_whitespace_on_save = 1
+vim.g.strip_whitelines_at_eof = 1
+vim.g.fzf_preview_window = {}
+
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#eb6f92 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#f6c177 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#9ccfd8 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#31748f gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#c4a7e7 gui=nocombine]]
+
+vim.opt.list = true
+vim.opt.listchars:append("space:·")
+vim.opt.listchars:append("eol:↴")
+
+require("indent_blankline").setup {
+  space_char_blankline ="",
+  char_highlight_list = {
+   "IndentBlanklineIndent1",
+   "IndentBlanklineIndent2",
+   "IndentBlanklineIndent3",
+   "IndentBlanklineIndent4",
+   "IndentBlanklineIndent5",
+  },
+}
+
+-- Set rose-pine-moon to color theme
+-- require('rose-pine').setup({
+	-- dark_variant = 'moon'
+-- })
+-- vim.cmd [[colorscheme rose-pine]]
+vim.cmd [[colorscheme catppuccin-mocha]]
+require("nvim-semantic-tokens").setup {
+  preset = "default",
+  -- highlighters is a list of modules following the interface of nvim-semantic-tokens.table-highlighter or
+  -- function with the signature: highlight_token(ctx, token, highlight) where
+  --        ctx (as defined in :h lsp-handler)
+  --        token  (as defined in :h vim.lsp.semantic_tokens.on_full())
+  --        highlight (a helper function that you can call (also multiple times) with the determined highlight group(s) as the only parameter)
+  highlighters = { require 'nvim-semantic-tokens.table-highlighter'}
+}
