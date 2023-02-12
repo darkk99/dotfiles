45b07ec7bed2179f7eb12ba9f6b6eb9e880becd3
darkk99 <colerabold@gmail.com>
2023-02-12 12:54:39 -0500

    change my bspwm config to lua

--- .config/polybar/scripts/brightness.bash
+++ .config/polybar/scripts/brightness.bash
@@ -1,4 +1,8 @@
-brightness=/sys/class/backlight/amdgpu_bl0/brightness
+if [ -f /sys/class/backlight/amdgpu_bl0/brightness ]; then
+  brightness=/sys/class/backlight/amdgpu_bl0/brightness
+else
+  brightness=/sys/class/backlight/amdgpu_bl1/brightness
+fi
 
 if [[ $1 == '+' ]]; then
   if [[ $(cat $brightness) != 255 ]]; then
